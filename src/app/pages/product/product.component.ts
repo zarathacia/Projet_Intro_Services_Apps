cf20bbb9be8b70be9054248d638361d59278cf65
eya-ml <eya.mlika@supcom.tn>
2022-02-23 22:18:10 +0100

    hello

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ItemService } from 'src/app/Services/ItemService';
 @Component({
   selector: 'app-item',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent {
-  constructor() {}
- }
+  itemId:any;
+  Item:any;
+  constructor(private route: ActivatedRoute,private itemService:ItemService ){}
+  ngOnInit() {
+    this.route.paramMap.subscribe(params =>{
+      this.itemId = params.get('ItemId');
+    });
+    this.getproductById(this.itemId);
+  }
+  getproductById(ItemId:any){
+    this.itemService.getItemById(ItemId).subscribe((res)=>{
+      this.Item = res;
+    });
+    console.log(this.Item);
+  }
+}
